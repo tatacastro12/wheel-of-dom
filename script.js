ec2e6d03313fc4bb2d5eb5ccbc38364c33023116
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2022-12-04 10:03:41 -0800

    from

--- script.js
+++ script.js
@@ -1,72 +1,72 @@
+// Tomar los elementos en el DOM donde se renderizan los datos
 const input = document.querySelector('#datos');
 const form = document.querySelector('.form')
 const ul = document.querySelector('.datos-resividos');
 
 
 
-let data = []
+let data = ""
+let users = []
 
-function delUser (){
-    
-    console.log('exe')
-    
-    // const userInList = e.target.parentElement
-
-    // userInList.remove()
-}
-
-
-
+/**
+ * Crea un elemento li por cada usuario nuevo y crea un boton 
+ * para eliminar el usuario de la lista  
+ */
 const addPlayer = ()=> {
+    // Crea los elementos li y span
+    const li = document.createElement('li')
+    const user = document.createElement("span")
+
+    // agregar clase player al li
+    li.classList.add('player')
+
+    user.textContent = '* ' + data
     
-    data.forEach(element => {
-        const li = document.createElement('li')
-        const user = document.createElement('span')
+    // agrega los elementos nuevos al ul
+    li.appendChild(user)
+    ul.appendChild(li)
 
-        const delBtn = document.createElement('button')
-        delBtn.textContent = 'Remove User'
-        delBtn.addEventListener('click', delUser)
+    // Del BTN
 
-        li.classList.add('player')
+    const delBtn = document.createElement("span");
+    delBtn.textContent = "Eliminar"
+    li.appendChild(delBtn)
 
-        user.textContent = '- ' + element.user
-        
-
-        li.appendChild(user)
-        li.appendChild(delBtn)
-
-        ul.appendChild(li)
-
-    })
-
-    
+    // remueve el li del usuario que se pretende eliminar
+    delBtn.onclick = () => {
+        li.remove();
+    }
     
 }
 
-const clearData = ()=> {
-    data = []
-}
-
-
+/**
+ * Se encarga de guardar los datos del usuario en el array users
+ * llama a la funcion addPlayer que muestra en el UL en pantalla
+ */
 const getInputData = (e)=> {
+    
+    data = e.target.value
 
-    e.preventDefault()
-
-    let user = e.target.value
-    let id = Date.now()
-
-    // data = [...data, {user, id}]
-
+    // * La funcion que muestra a los usuarios registrados 
+    // * en la UL
     addPlayer()
 
-    form.reset()
+    // ! Captura los datos de los usuarios registrados
+    // ! No es necesario por ahora
+    users = [...users, {user: e.target.value, id: Date.now() }]
 
+    // * Limpia el input una ves que se ingresa un usuario
+    form.reset()
 
 }
 
 
+// * Se mantiene a la escucha de los cambios en el input y llama
+// * a la funcion getInputData
 input.addEventListener('change', getInputData)
 
+
+// * Evita que la pagina se recargue cuando se envian los datos del formulario
 form.addEventListener('submit', (e)=> {
     e.preventDefault()
 })

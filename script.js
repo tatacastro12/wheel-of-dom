09d79f652db656c4d59f35d2ce54ce8073b0c627
dianamorenodev <valentinamoreno2002@gmail.com>
2022-12-05 16:13:29 -0500

    Ajuste de backgrounds

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let users = []
  * Crea un elemento li por cada usuario nuevo y crea un boton 
  * para eliminar el usuario de la lista  
  */
-const addPlayer = ()=> {
+const addPlayer = () => {
     // Crea los elementos li y span
     const li = document.createElement('li')
     const user = document.createElement("span")
@@ -21,7 +21,7 @@ const addPlayer = ()=> {
     li.classList.add('player')
 
     user.textContent = '* ' + data
-    
+
     // agrega los elementos nuevos al ul
     li.appendChild(user)
     ul.appendChild(li)
@@ -36,15 +36,15 @@ const addPlayer = ()=> {
     delBtn.onclick = () => {
         li.remove();
     }
-    
+
 }
 
 /**
  * Se encarga de guardar los datos del usuario en el array users
  * llama a la funcion addPlayer que muestra en el UL en pantalla
  */
-const getInputData = (e)=> {
-    
+const getInputData = (e) => {
+
     data = e.target.value
 
     // * La funcion que muestra a los usuarios registrados 
@@ -53,7 +53,7 @@ const getInputData = (e)=> {
 
     // ! Captura los datos de los usuarios registrados
     // ! No es necesario por ahora
-    users = [...users, {user: e.target.value, id: Date.now() }]
+    users = [...users, { user: e.target.value, id: Date.now() }]
 
     // * Limpia el input una ves que se ingresa un usuario
     form.reset()
@@ -67,8 +67,6 @@ input.addEventListener('change', getInputData)
 
 
 // * Evita que la pagina se recargue cuando se envian los datos del formulario
-form.addEventListener('submit', (e)=> {
+form.addEventListener('submit', (e) => {
     e.preventDefault()
 })
-
-
